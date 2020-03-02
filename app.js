6a2a41fa3179e526933c5ed896963f051ce48ea6
GhostsAg <Andres.Ledesma9511@gmail.com>
2020-03-02 16:39:05 -0600

    fixed unknown token

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-// const OUTPUT_DIR = path.resolve(__dirname, "output")
-// const outputPath = path.join(OUTPUT_DIR, "team.html");
+const OUTPUT_DIR = path.resolve("./output/", "output")
+const outputPath = path.join("./output/", "team.html");
 const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
@@ -63,7 +63,9 @@ inquirer.prompt([
     const engineer = new Engineer(response.engineerName, response.engineerId, response.engineerEmail, response.engineerGit);
     const intern = new Intern(response.internName, response.internId, response.internEmail, response.internSchool);
     const manager = new Manager(response.managerName, response.managerId, response.managerEmail, response.man);
-    //render();
+
+    const employees = [engineer, intern, manager];
+    render(employees);
 });
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
